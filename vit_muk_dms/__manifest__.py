73c5c110a4521522c7eddf14324643d16a93b5b3
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2019-12-18 12:51:51 +0700

    report file review

--- vit_muk_dms/__manifest__.py
+++ vit_muk_dms/__manifest__.py
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','muk_dms'],
+    'depends': ['base','report','muk_dms'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'reports/file.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
